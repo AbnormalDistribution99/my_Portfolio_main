91288be58d9974fdca3ed123d606889fa82fd4b5
AbnormalDistribution99 <ar2818@msstate.edu>
2022-09-09 19:13:45 -0500

    added resume

--- src/components/NavBar.jsx
+++ src/components/NavBar.jsx
@@ -31,16 +31,21 @@ const NavBar = () => {
             </Link>
         </li>
         <li>
+          <a href="https://drive.google.com/file/d/1f7FHlZdHLl3umKfbRSrnI1rmiQ6Blpm4/view?usp=sharing">Resume</a>
+        </li>
+        <li>
             <Link to="contact" spy={true} smooth={true} duration={500}>
               Contact
             </Link>
         </li>
         <li>Playground</li>
         </ul>
+
     {/* Hamburger */}
     <div className='md:hidden z-10' onClick={handleClick}>
         {!nav ? <FaBars /> : <FaTimes />}
     </div>
+
     {/* Mobile Menu */}
     <ul className={!nav ? 'hidden': 'absolute top-0 left-0 w-full h-screen bg-[black] flex flex-col justify-center items-center'}>
         <li className='py-6 text-4xl'>
@@ -58,13 +63,17 @@ const NavBar = () => {
               Skills
             </Link>
         </li>
-        <li className='py-6 text-4xl'>Resume</li>
+        <li className='py-6 text-4xl'>
+          <a href="https://drive.google.com/file/d/1f7FHlZdHLl3umKfbRSrnI1rmiQ6Blpm4/view?usp=sharing">Resume</a>
+        </li>
         <li className='py-6 text-4xl'>
             <Link onClick={handleClick} to="contact" spy={true} smooth={true} duration={500}>
               Contact
             </Link>
         </li>
-        <li className='py-6 text-4xl'>Playground</li>
+        <li className='py-6 text-4xl'>
+          Playground
+        </li>
     </ul>
     <div>
         <img src={Logo} alt="Logo Image" style={{width: '150px'}}/>
